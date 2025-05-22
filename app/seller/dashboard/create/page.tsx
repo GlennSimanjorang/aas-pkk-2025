b36bbc9af304554ed880aa6dab8a271a3c7d6017
Ucokkks <glenmartua@gmail.com>
2025-05-22 13:41:37 +0700

    feat: seller:create

---
+++ app/seller/dashboard/create/page.tsx
@@ -0,0 +1,228 @@
+"use client";
+
+import { useEffect } from "react";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { getCookie } from "cookies-next";
+import { useState } from "react";
+
+const formSchema = z.object({
+  name: z.string().min(1, "Nama produk harus diisi"),
+  imageFile: z.instanceof(File, { message: "Gambar produk harus diupload" }),
+  price: z.coerce.number().min(1, "Harga harus diisi"),
+  stock: z.coerce.number().min(1, "Stok harus diisi"),
+  sub_sub_category_id: z.coerce.number().min(1, "Kategori harus dipilih"),
+});
+
+export default function ProductForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      price: 0,
+      stock: 0,
+      sub_sub_category_id: undefined,
+      imageFile: undefined,
+    },
+  });
+
+  const router = useRouter();
+  const [categories, setCategories] = useState<
+    Array<{ id: number; name: string }>
+  >([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCategories() {
+      setIsLoading(true);
+      try {
+        let allCategories: Array<{ id: number; name: string }> = [];
+        let nextPageUrl = "http://localhost:8000/api/sub-sub-categories";
+
+        while (nextPageUrl) {
+          const res = await axios.get(nextPageUrl);
+          const responseData = res.data.content;
+
+          allCategories = [...allCategories, ...responseData.data];
+          nextPageUrl = responseData.next_page_url;
+        }
+
+        setCategories(allCategories);
+      } catch (err) {
+        console.error("Gagal mengambil kategori", err);
+        toast.error("Gagal memuat kategori");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCategories();
+  }, []);
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const formData = new FormData();
+      formData.append("name", values.name);
+      formData.append("imageFile", values.imageFile);
+      formData.append("price", values.price.toString());
+      formData.append("stock", values.stock.toString());
+      formData.append(
+        "sub_sub_category_id",
+        values.sub_sub_category_id.toString()
+      );
+
+      const token = getCookie("token");
+      await axios.post("http://localhost:8000/api/seller/products", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      toast.success("Produk berhasil ditambahkan");
+      router.push("/seller/dashboard");
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Gagal menambahkan produk");
+    }
+  }
+
+  return (
+    <div className="flex flex-1 flex-col p-4 pt-6 max-w-lg mx-auto">
+      <h1 className="text-xl font-bold py-4">Tambah Produk Baru</h1>
+      <div className="rounded-xl bg-muted/90 border dark:border-none dark:bg-muted/50 p-8">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 max-w-3xl mx-auto py-10"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nama Produk</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Nama Produk" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="imageFile"
+              render={({ field: { onChange, value, ...fieldProps } }) => (
+                <FormItem>
+                  <FormLabel>Gambar Produk</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="file"
+                      accept="image/*"
+                      onChange={(e) => onChange(e.target.files?.[0])}
+                      {...fieldProps}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Harga</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="10000"
+                      {...field}
+                      onChange={(e) => field.onChange(parseInt(e.target.value))}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="stock"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Stok</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="10"
+                      {...field}
+                      onChange={(e) => field.onChange(parseInt(e.target.value))}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="sub_sub_category_id"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Kategori</FormLabel>
+                  <Select
+                    onValueChange={(value) => field.onChange(Number(value))}
+                    value={field.value?.toString()}
+                    disabled={isLoading}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Pilih Sub Sub Kategori" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {categories.map((cat) => (
+                        <SelectItem key={cat.id} value={cat.id.toString()}>
+                          {cat.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" disabled={isLoading}>
+              {isLoading ? "Memproses..." : "Submit"}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
--- app/seller/dashboard/page.tsx
+++ app/seller/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { AppSidebar } from "@/components/app-sidebar"
 import {
   Breadcrumb,
@@ -13,10 +15,56 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button"
+import {
+  Plus
+} from "lucide-react";
+import axios from "axios"
+import { useEffect, useState} from "react";
+import { getCookie } from "cookies-next"
+import { toast, ToastContainer } from "react-toastify";
+
+interface product {
+  id: string;
+  name: string;
+  image: string;
+  price: number;
+  stock: number;
+  slug: string;
+}
 
 export default function Page() {
+  const [data, setData] = useState<product[]>([]);
+
+  useEffect(() => {
+    const token = getCookie("token");
+    const FetchProduct = async() => {
+      try {
+        const respone = await axios.get(
+          "http://localhost:8000/api/seller/products",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setData(respone.data.content.data)
+      } catch  {
+        toast.error("Gagal mengambil data", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+        });
+      }
+    }
+    FetchProduct();
+  }, [])
+
+
   return (
     <SidebarProvider>
+      <ToastContainer />
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
@@ -29,9 +77,7 @@ export default function Page() {
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="">
-                    Seller Stats
-                  </BreadcrumbLink>
+                  <BreadcrumbLink href="">Seller Stats</BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
@@ -41,17 +87,35 @@ export default function Page() {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl">
-              
+        <div className="">
+          <Button className="ml-20 mt-5">
+            <Plus className="w-4 h-4" />
+            <p className="text-white font-medium text-sm">
+              <a href="/seller/dashboard/create">Tambah Produk</a>
+            </p>
+          </Button>
+        </div>
+        <div className="mt-10 flex gap-4 ml-20">
+          {/* Daftar Item */}
+          {data.map((item) => (
+            <div
+              key={item.slug}
+              className="w-44 h-80  bg-white rounded-lg items-center gap-5 drop-shadow-lg"
+            >
+              <img
+                src={item.image}
+                alt={item.name}
+                className=" object-cover aspect-square rounded lg mb-4"
+              />
+              <p className="font-medium text-sm ml-3">{item.name}</p>
+              <p className="font-bold text-base ml-3 mt-2">Rp.{item.price}</p>
+              <p className="font-medium text-sm ml-3 text-gray-400 ">
+                stock:{item.stock}
+              </p>
             </div>
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          ))}
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
